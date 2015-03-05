6e2d41f0d4730e516e8bd398be12dcb436f9a39d
bestan <bestan93@gmail.com>
2015-03-05 20:40:38 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+var FieldVal = require('fieldval');
+var BasicVal = FieldVal.BasicVal;
+ 
+var express = require('express');
+var bodyParser = require('body-parser');
+ 
+var app = express();
+app.use(bodyParser.json());
+app.use(express.static('./public'))
+
+app.post('/sign_up', function (req, res) {
+    
+    var validator = new FieldVal(req.body);
+    validator.get_async("email", [BasicVal.email(), function(value, emit, callback) {
+    	//Imitating asynchronous database call
+    	setTimeout(function() {
+    		callback({
+    			error: 1001,
+    			error_message: "Email already exists"
+    		})
+    	}, 5);
+    }]);
+    
+    var password = validator.get("password", BasicVal.string(), BasicVal.min_length(8))
+    
+    //Performing validation on password_repeat only if password was provided
+    if (password) {
+    	validator.get("password_repeat", function(value) {
+    		if (password != value) {
+    			return {
+    				error: 1000,
+    				error_message: "Password does not match"
+    			}
+    		}
+    	})
+    }
+
+    //Validating nested object
+    validator.get("address", BasicVal.object(), function(value) {
+    	var inner_validator = new FieldVal(value);
+    	inner_validator.get("street", BasicVal.string());
+    	inner_validator.get("city", BasicVal.string());
+    	inner_validator.get("postal_code", BasicVal.string());
+    	return inner_validator.end();
+    });
+    
+    validator.end(function(error) {
+	    if (error) {
+	        res.json(error);
+	     } else {
+		    res.json({success:true})	
+	     }
+    });
+    
+})
+ 
+app.listen(3000);
