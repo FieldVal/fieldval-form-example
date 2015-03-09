a27b42cabdf2820e38558033cf67096422877df4
bestan <bestan93@gmail.com>
2015-03-09 14:06:52 +0200

    Updated validation

--- app.js
+++ app.js
@@ -11,20 +11,11 @@ app.use(express.static('./public'))
 app.post('/sign_up', function (req, res) {
     
     var validator = new FieldVal(req.body);
-    validator.get_async("email", [BasicVal.email(), function(value, emit, callback) {
-    	//Imitating asynchronous database call
-    	setTimeout(function() {
-    		callback({
-    			error: 1001,
-    			error_message: "Email already exists"
-    		})
-    	}, 5);
-    }]);
-    
-    var password = validator.get("password", BasicVal.string(), BasicVal.min_length(8))
-    
-    //Performing validation on password_repeat only if password was provided
-	validator.get("password_repeat", function(value) {
+   
+    //Password validation
+    var password = validator.get("password", BasicVal.string(), BasicVal.min_length(8))    
+	validator.get("password_repeat", BasicVal.string(), function(value) {
+		//Validating "password_repeat" only if password was provided
 		if (password && password != value) {
 			return {
 				error: 1000,
@@ -33,9 +24,10 @@ app.post('/sign_up', function (req, res) {
 		}
 	})
 
+    //Date of birth validation
 	validator.get('date_of_birth', BasicVal.date('yyyy-MM-dd'));
 
-    //Validating nested object
+    //Address object validation
     validator.get("address", BasicVal.object(), function(value) {
     	var inner_validator = new FieldVal(value);
     	inner_validator.get("street", BasicVal.string());
@@ -43,6 +35,17 @@ app.post('/sign_up', function (req, res) {
     	inner_validator.get("postal_code", BasicVal.string());
     	return inner_validator.end();
     });
+
+    //Email validation
+    validator.get_async("email", [BasicVal.email(), function(value, emit, callback) {
+        //Imitating asynchronous database call
+        setTimeout(function() {
+            callback({
+                error: 1001,
+                error_message: "Email already exists"
+            })
+        }, 5);
+    }]);
     
     validator.end(function(error) {
 	    if (error) {
@@ -53,5 +56,9 @@ app.post('/sign_up', function (req, res) {
     });
     
 })
- 
-app.listen(3000);
+
+app.set('port', process.env.PORT || 3000); 
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+
+});
